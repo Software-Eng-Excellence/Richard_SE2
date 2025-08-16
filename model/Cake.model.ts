18c248e341632421175e0245dff94b6ec887e1d0
Richard Geagea <geagear3@gmail.com>
2025-08-16 08:50:33 +0300

    Add models

---
+++ model/Cake.model.ts
@@ -0,0 +1,81 @@
+import { Item, ItemCategory } from "./item.model";
+
+export class CAKE implements Item {
+  
+    constructor(
+        id: number,
+        type: string,
+        flavor: string,
+        filling: string,
+        size: string,
+        layers: number,
+        frostingType: string,
+        frostingFlavor: string,
+        decoration: string,
+        notes: string
+    ) {
+        this.id = id;
+        this.type = type;
+        this.flavor = flavor;
+        this.filling = filling;
+        this.size = size;
+        this.layers = layers;
+        this.frostingType = frostingType;
+        this.frostingFlavor = frostingFlavor;
+        this.decoration = decoration;
+        this.notes = notes;
+    }
+    private id: number;
+    private type: string;
+    private flavor: string;
+    private filling: string;
+    private size: string;
+    private layers: number;
+    private frostingType: string;
+    private frostingFlavor: string;
+    private decoration: string;
+    private notes: string;
+      getCategory(): ItemCategory {
+        return ItemCategory.CAKE;
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    getType(): string {
+        return this.type;
+    }
+
+    getFlavor(): string {
+        return this.flavor;
+    }
+
+    getFilling(): string {
+        return this.filling;
+    }
+
+    getSize(): string {
+        return this.size;
+    }
+
+    getLayers(): number {
+        return this.layers;
+    }
+
+    getFrostingType(): string {
+        return this.frostingType;
+    }
+
+    getFrostingFlavor(): string {
+        return this.frostingFlavor;
+    }
+
+    getDecoration(): string {
+        return this.decoration;
+    }
+
+    getNotes(): string {
+        return this.notes;
+    }
+}   
